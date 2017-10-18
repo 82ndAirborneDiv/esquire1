b2ffe98e2885d339e6767b5bcb17975e58fc9fd7
Kristin Muterspaw <kmmuterspaw@gmail.com>
2017-10-18 10:10:02 -0400

    Create docker files and scripts to run a docker environment. Can only (somewhat) guarantee that the dev instance works.

---
+++ esquire1/start_docker_dev.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+echo "Make sure your Docker setup is set to 4GB of memory. This is needing for installing SQL Server"
+docker build -f dockerfiles/Dockerfile-build -t esquire/esquire-build .
+docker build -f dockerfiles/Dockerfile-runtime -t esquire/esquire-runtime .
+docker build -f dockerfiles/Dockerfile -t esquire/app .
+docker run -p 5000:5000 -p 27017:27017 -p 1433:1433 -v $(pwd)/ClientApp:/app/ClientApp --name esquire esquire/app
--- esquire1/start_docker_prod.sh
+++ esquire1/start_docker_prod.sh
@@ -2,5 +2,5 @@
 
 docker build -f dockerfiles/Dockerfile-build -t esquire/esquire-build .
 docker build -f dockerfiles/Dockerfile-runtime -t esquire/esquire-runtime .
-docker build -f dockerfiles/Dockerfile -t esquire/app .
+docker build -f dockerfiles/Dockerfile-prod -t esquire/app .
 docker run -p 5000:5000 -p 27017:27017 -p 1433:1433 --name esquire esquire/app
