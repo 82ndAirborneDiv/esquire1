b181da7c00191a7b5510a548272403a0413b2b1a
Kristin Muterspaw <kmmuterspaw@gmail.com>
2017-10-23 15:12:02 -0400

    Add a simulate test to the Home scene suite

--- esquire1/ClientApp/src/scenes/Home/__tests__/Home.test.js
+++ esquire1/ClientApp/src/scenes/Home/__tests__/Home.test.js
@@ -2,17 +2,25 @@ import React from 'react'
 import { shallow } from 'enzyme'
 import { Home } from '../'
 
+const props = {
+  actions: {
+    getProjectsRequest: jest.fn(),
+    addProjectRequest: jest.fn()
+  },
+  projects: [
+    { name: 'Project 1' },
+    { name: 'Project 2' }
+  ]
+}
+
 describe('Home scene', () => {
   test('it should render correctly', () => {
-    const props = {
-      actions: {
-        getProjectsRequest: jest.fn()
-      },
-      projects: [
-        { name: 'Project 1' },
-        { name: 'Project 2' }
-      ]
-    }
     expect(shallow(<Home {...props} />)).toMatchSnapshot()
+  })
+  
+  test('it should handle ADD PROJECT button click event', () => {
+    let wrapper = shallow(<Home {...props} />)
+    wrapper.find('MainButton').simulate('click')
+    expect(props.actions.addProjectRequest).toHaveBeenCalled()
   })
 })
