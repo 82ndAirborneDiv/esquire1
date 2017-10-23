cc244b0b9b6d759532e79dbc0c7b80f4ceb9a119
Kristin Muterspaw <kmmuterspaw@gmail.com>
2017-10-23 13:49:59 -0400

    Update docker dev script to have a volume for mongoDB

--- esquire1/start_docker_dev.sh
+++ esquire1/start_docker_dev.sh
@@ -3,4 +3,4 @@ echo "Make sure your Docker setup is set to 4GB of memory. This is needing for i
 docker build -f dockerfiles/Dockerfile-build -t esquire/esquire-build .
 docker build -f dockerfiles/Dockerfile-runtime -t esquire/esquire-runtime .
 docker build -f dockerfiles/Dockerfile -t esquire/app .
-docker run -p 5000:5000 -p 27017:27017 -p 1433:1433 -v $(pwd)/ClientApp:/app/ClientApp -v sqlvolume:/var/opt/mssql --name esquire esquire/app
+docker run -p 5000:5000 -p 27017:27017 -p 1433:1433 -v $(pwd)/ClientApp:/app/ClientApp -v sqlvolume:/var/opt/mssql -v mongostore:/data/db --name esquire esquire/app
