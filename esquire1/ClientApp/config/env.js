4e54b90aa3d8ce540e5a78c7090db3dd7f614eb3
Kristin Muterspaw <kmmuterspaw@gmail.com>
2017-10-26 09:27:28 -0400

    Updates include defaults for variables if the .env file doesn't exist and only use Redux devtools extension if it exists, update to production dockerfile

--- esquire1/ClientApp/config/env.js
+++ esquire1/ClientApp/config/env.js
@@ -4,10 +4,11 @@ const paths = require('./paths')
 module.exports = function getEnvVariables(nodeEnv) {
   dotenv.config({ path: paths.appDotEnv })
 
+  // Node env is lower case, while asp.net core is upper case, so capitalize first letter
   const aspEnv = nodeEnv.replace(nodeEnv.charAt(0), nodeEnv.charAt(0).toUpperCase())
   process.env.NODE_ENV = nodeEnv
   process.env.ASPNETCORE_ENVIRONMENT = aspEnv
-
+  
   let env = Object.keys(process.env)
     .filter(key => key.startsWith('APP_'))
     .reduce((e, key) => {
@@ -15,6 +16,7 @@ module.exports = function getEnvVariables(nodeEnv) {
       return e
     }, {})
   env = { ...env, ASPNETCORE_ENVIRONMENT: `"${aspEnv}"`, NODE_ENV: `"${nodeEnv}"` }
+  if (!env.APP_API_URL) { env.APP_API_URL = JSON.stringify('http://localhost:5000/api') }
 
   return env
 }
--- esquire1/ClientApp/src/services/store/index.js
+++ esquire1/ClientApp/src/services/store/index.js
@@ -4,13 +4,13 @@ import projectLogic from 'scenes/Home/logic'
 import rootReducer from 'reducer'
 
 const INITIAL_STATE = {}
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
 
 const store = createStore(
   rootReducer,
   INITIAL_STATE,
-  compose(
-    applyMiddleware(createLogicMiddleware(projectLogic)),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  composeEnhancers(
+    applyMiddleware(createLogicMiddleware(projectLogic))
   )
 );
 
